4ca048233cd2cd6029ccf1e48eb752df62f91887
Daniil Iskhakov <shkiper325@gmail.com>
2023-03-03 00:28:01 +0300

    Sync

--- main.cpp
+++ main.cpp
@@ -1,13 +1,33 @@
+#include <iostream>
+
 #include "runge.hpp"
 #include "utils.hpp"
 #include "newton.hpp"
 
-int main() {
+using namespace std;
+
+int main(int argc, char *argv[]) {
     // test_runge5_variable_step();
     // test_jacobian_and_inverse3x3();
 
+    if (argc != 3) {
+        cerr << "Argument count != 2" << endl;
+        exit(1);
+    }
+
+    double alpha;
+    int step_count;
+    try {
+        alpha = atof(argv[1]);
+        step_count = atoi(argv[2]);
+    }
+    catch (...) {
+        cerr << "Cant read cmd args" << endl;
+        exit(1);
+    }
+
     main_routine(
-        0,
+        alpha,
         0.1,
         0.1,
         0.1,
@@ -15,7 +35,8 @@ int main() {
         1e-5,
         10,
         1e-7,
-        1e-7
+        1e-7,
+        step_count
     );
 
     return 0;
